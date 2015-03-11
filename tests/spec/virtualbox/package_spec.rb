de1d7a150790a6f4c4df95d7d322330167e427a1
N'Diaye Abdoul <abdoul.nd@gmail.com>
2015-03-11 11:16:34 +0000

    Add server specs

--- tests/spec/virtualbox/package_spec.rb
+++ tests/spec/virtualbox/package_spec.rb
@@ -1,9 +1,14 @@
 require 'spec_helper'
 
-describe package('deamon') do
+describe package('daemon') do
   it { should be_installed }
 end
 
 describe package('heka') do
   it { should be_installed }
+end
+
+describe service('hekad') do
+  it { should be_enabled }
+  it { should be_running }
 end
--- tests/spec/virtualbox/port_spec.rb
+++ tests/spec/virtualbox/port_spec.rb
@@ -3,3 +3,7 @@ require 'spec_helper'
 describe port(4352) do
   it { should be_listening }
 end
+
+describe port(5565) do
+  it { should be_listening }
+end
